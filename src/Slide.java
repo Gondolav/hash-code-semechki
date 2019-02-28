5dfb7a93f1ae3497e88480c5f0981b11b30fd74b
LudoHoff <ludo.hoffstetter@epfl.ch>
2019-02-28 19:24:43 +0100

    Add Slide classes

---
+++ src/Slide.java
@@ -0,0 +1,21 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public abstract class Slide {
+    public abstract Set<String> getTags();
+    public int scoreWith(Slide that) {
+        return Math.min(this.commonTags(that), Math.min(this.tagsNotIn(that), that.tagsNotIn(this)));
+    }
+
+    public int tagsNotIn(Slide that) {
+        Set<String> copies = new TreeSet<>(getTags());
+        copies.removeAll(that.getTags());
+        return copies.size();
+    }
+
+    public int commonTags(Slide that) {
+        Set<String> copies = new TreeSet<>(getTags());
+        copies.retainAll(that.getTags());
+        return copies.size();
+    }
+}
---
+++ src/VerticalSlide.java
@@ -0,0 +1,28 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class VerticalSlide extends Slide {
+
+    private VerticalPhoto leftPhoto;
+    private VerticalPhoto rightPhoto;
+
+    public VerticalSlide(VerticalPhoto leftPhoto, VerticalPhoto rightPhoto) {
+        this.leftPhoto = leftPhoto;
+        this.rightPhoto = rightPhoto;
+    }
+
+    @Override
+    public Set<String> getTags() {
+        Set<String> union = leftPhoto.getTags();
+        union.addAll(rightPhoto.getTags());
+        return union;
+    }
+
+    public VerticalPhoto getLeftPhoto() {
+        return leftPhoto;
+    }
+
+    public VerticalPhoto getRightPhoto() {
+        return rightPhoto;
+    }
+}
